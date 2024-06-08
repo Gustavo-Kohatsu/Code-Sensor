a2c07e686b5ca276d18a427e3fba6af7acc17458
Igor <igor.jimenez@sptech.school>
2024-06-08 12:06:08 -0300

    cadastro do funcionario

--- API-BD/src/models/usuarioModel.js
+++ API-BD/src/models/usuarioModel.js
@@ -23,7 +23,19 @@ function cadastrar(nome, email, telefone, cnpj, cep, senha, filial) {
     return database.executar(instrucaoSql);
 }
 
+function cadastrarFunc(nome, email, cpf, tipo, senha, fkEmpresa) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarFunc():", nome, email, senha);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO funcionario (nome, email, cpf, tipo, senha,fkEmpresa) VALUES ('${nome}', '${email}', '${cpf}','${tipo}','${senha}','${fkEmpresa}');`;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
     autenticar,
-    cadastrar
+    cadastrar,
+    cadastrarFunc
 };
--- API-BD/src/routes/usuarios.js
+++ API-BD/src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.post("/cadastrarFunc/:fkEmpresa", function (req, res) {
+    usuarioController.cadastrarFunc(req, res);
+});
+
 module.exports = router;
